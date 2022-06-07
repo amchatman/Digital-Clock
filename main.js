77200b018468b8ecdcac01e572617abe0c48f9d7
amchatman <ashchat2008@gmail.com>
2022-06-07 15:49:41 -0400

    Update
    --Html and JavaScript update

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
+function currentTime(){
 
+// Get Date, Month, Time & etc
 let currentDate = new Date();
 let day = currentDate.getDay();
 let month = currentDate.getMonth();
+let date = currentDate.getDate();
+let year = currentDate.getFullYear();
+let hour = currentDate.getHours();
+let mintues = currentDate.getMinutes();
+let seconds = currentDate.getSeconds();
 
-let dayNames = ['Sunday','Monday','Tuesday','Wednesday'];
+//Array for Day and Months
+let dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thurday','Friday','Saturday'];
 let monthNames = ['January','Feburary','March','April','May','June','July'];
 
 
-document.getElementById('currentDay').innerHTML = dayNames[day];
-document.getElementById('currentMonth').innerHTML = monthNames[month];
+let thisMonth  = monthNames[month] + "," + date + " " + year;
+let thisTime = hour + ":" +  mintues + ":" + seconds;
 
+document.getElementById('currentDay').innerHTML = dayNames[day];
+document.getElementById('currentMonth').innerHTML = thisMonth;
+document.getElementById('currentTime').innerHTML = thisTime;
+
+let t = setTimeout(function(){ currentTime() }, 1000);
+}
+currentTime();

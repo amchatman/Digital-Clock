d6066f753c0f6ed53dc3a9346fac83736c16712a
amchatman <ashchat2008@gmail.com>
2022-06-09 14:08:04 -0400

    Light & Dark mode
    --Add light & dark mode with CSS
    --Remove change format button
    --Change Hex colors to variables

--- main.js
+++ main.js
@@ -11,8 +11,9 @@ let mintues = currentDate.getMinutes();
 let seconds = currentDate.getSeconds();
 let period = "AM";
 
+
 //Array for Day and Months
-let dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thurday','Friday','Saturday'];
+let dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 let monthNames = ['January','Feburary','March','April','May','June','July'];
 
 
@@ -21,9 +22,21 @@ let thisMonth  = monthNames[month] + " " + date + ", " + year;
 if(hour >= 12){
     period = "PM";
 }
+
+//Change the format from 24 hour to 12 hour
 hour = hour > 12 ? hour % 12 : hour;
 
+
+//Add a 0 in from of numbers less then 10 on clock
+if(mintues < 10){
+    mintues = "0" + mintues;
+}
+if (seconds < 10){
+    seconds = "0" + seconds;
+}
+//Time format
 let thisTime = hour + " : " +  mintues + " : " + seconds + " " + period;
+
 
 document.getElementById('currentDay').innerHTML = dayNames[day];
 document.getElementById('currentMonth').innerHTML = thisMonth;
@@ -31,4 +44,5 @@ document.getElementById('currentTime').innerHTML = thisTime;
 
 let t = setTimeout(function(){ currentTime() }, 1000);
 }
+
 currentTime();

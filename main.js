1f3ea7032395cb02bdde9268d646e7ba1b236a41
amchatman <ashchat2008@gmail.com>
2022-06-13 16:17:12 -0400

    Light/Dark mode
    --Added Light mode and dark mode features
    --Add light mode button

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
+//Get current Day, Month, Date and Year
 var currentDate = new Date();
 var day = currentDate.getDay();
 var month = currentDate.getMonth();
 var date = currentDate.getDate();
 var year = currentDate.getFullYear();
 
-
+//Array stores days of the week and months
 let dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 let monthNames = ['January','Feburary','March','April','May','June','July'];
+
+//Sets the date format as: Month Day, Year
 let thisMonth  = monthNames[month] + " " + date + ", " + year;
 
+//Display day and month
 document.getElementById('currentDay').innerHTML = dayNames[day];
 document.getElementById('currentMonth').innerHTML = thisMonth;
 
+//Function that gets the current time and switchs from the 24 hour clock to 12 hour
 var twentyFourHour = false,
 thisTime = document.getElementById("currentTime"),
 changeButton = document.getElementById("changeFormat");
@@ -26,6 +31,7 @@ var chanageTime = function (){
         var period = hours >= 12 ? "PM" : "AM";
         zero = seconds < 10 ? "0": " ",
         zeroMin = mintues < 10 ? "0": " ";
+        
         thisTime.innerText = hours + " : " + zeroMin + mintues + " : " + zero + seconds + " " + period; 
         
     }
@@ -40,11 +46,15 @@ var chanageTime = function (){
 
 }; 
 
-
-
 var changeFormat = function() {
     twentyFourHour = !twentyFourHour;
 };
+
+const lightbtn = document.querySelector('.light-mode-icon');
+lightbtn.addEventListener("click", function() { 
+    document.body.classList.toggle("light-theme");
+  });
+ 
 
 changeButton.addEventListener("click", changeFormat);
 window.setInterval(chanageTime, 100);
